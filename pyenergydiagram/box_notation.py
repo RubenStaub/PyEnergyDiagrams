615b63db18e940306345f1cd97b615a2bff0a0c0
Ruben Staub <ruben.staub@ens-lyon.fr>
2020-02-01 20:09:17 +0100

    Add print compatibility with Python3

--- pyenergydiagram/box_notation.py
+++ pyenergydiagram/box_notation.py
@@ -5,6 +5,7 @@ Created on Sat Jan  6 21:01:16 2018
 @author: giacomo
 """
 
+from __future__ import print_function
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
@@ -72,7 +73,7 @@ def plot_orbital_boxes (ax,x,y,boxes_number, electrons_number, box_side = 1, spa
     #plot the spins using Aufbau
     if electrons_number > 0:
         moduloelectrons = electrons_number%boxes_number
-        print moduloelectrons
+        print(moduloelectrons)
         if moduloelectrons > boxes_number:
             Warning ("electrons_number grater than boxes number")
         if electrons_number <= boxes_number:
